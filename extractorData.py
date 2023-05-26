9d31c5bba2e3c725732d6ca45e3c7f581e270609
pakitowka <whitesaint12@gmail.com>
2023-05-26 05:04:36 +0200

    scrapping done

--- extractorData.py
+++ extractorData.py
@@ -2,12 +2,10 @@ import requests
 import selectorlib as sl
 
 URL = "http://programmer100.pythonanywhere.com/"
-HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 
-def scrape(url=URL, headers=HEADERS):
-    response = requests.get(url,headers)
+def scrape(url=URL):
+    response = requests.get(url)
     content = response.text
     return content
 
